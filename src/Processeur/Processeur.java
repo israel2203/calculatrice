d779dcb98050c4b11773d61505d2cde1bfe00135
israel2203 <israelkipoko@gmail.com>
2020-03-27 20:54:42 +0000

    first commit

---
+++ src/Processeur/Processeur.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Processeur;
+
+import Interface.Fenetre;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author israel
+ */
+public class Processeur {
+    
+    private Fenetre screen= new Fenetre();
+    
+  
+    
+   
+    
+    
+     
+
+    
+}
